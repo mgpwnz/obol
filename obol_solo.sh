061d300bdcc698ea623181db98f21a5dc0aebebc
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-02-02 01:31:15 +0200

    Update obol_solo.sh

--- obol_solo.sh
+++ obol_solo.sh
@@ -20,49 +20,28 @@ echo "============================================================"
 # set vars
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt install make clang pkg-config libssl-dev libclang-dev build-essential git curl ntp jq llvm tmux htop screen unzip -y
-apt install docker-compose -y
+
 install() {
+	# скачиваем docker
+	sudo apt install docker.io -y
+
+	# устанавливаем docker
+	git clone https://github.com/docker/compose
+
+	# переходим в папку compose
+	cd compose
+
+	# копируем файл в рабочую директорию
+	git checkout v2.6.1
+
+	# преобразуем файлы
+	make
+
+	# переходим в корневую папку сервера
 	cd
-	if ! docker --version; then
-		echo -e "${C_LGn}Docker installation...${RES}"
-		sudo apt update && sudo apt upgrade -y
-		sudo apt install curl apt-transport-https ca-certificates gnupg lsb-release -y
-		. /etc/*-release
-		wget -qO- "https://download.docker.com/linux/${DISTRIB_ID,,}/gpg" | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-		echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-		sudo apt update
-		sudo apt install docker-ce docker-ce-cli containerd.io -y
-		docker_version=`apt-cache madison docker-ce | grep -oPm1 "(?<=docker-ce \| )([^_]+)(?= \| https)"`
-		sudo apt install docker-ce="$docker_version" docker-ce-cli="$docker_version" containerd.io -y
-	fi
-	if ! docker-compose --version; then
-		echo -e "${C_LGn}Docker Сompose installation...${RES}"
-		sudo apt update && sudo apt upgrade -y
-		sudo apt install wget jq -y
-		local docker_compose_version=`wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name"`
-		sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-`uname -s`-`uname -m`"
-		sudo chmod +x /usr/bin/docker-compose
-		. $HOME/.bash_profile
-	fi
-	if [ "$dive" = "true" ] && ! dpkg -s dive | grep -q "ok installed"; then
-		echo -e "${C_LGn}Dive installation...${RES}"
-		wget https://github.com/wagoodman/dive/releases/download/v0.9.2/dive_0.9.2_linux_amd64.deb
-		sudo apt install ./dive_0.9.2_linux_amd64.deb
-		rm -rf dive_0.9.2_linux_amd64.deb
-	fi
-}
-uninstall() {
-	echo -e "${C_LGn}Docker uninstalling...${RES}"
-	sudo dpkg -r dive
-	sudo systemctl stop docker.service docker.socket
-	sudo systemctl disable docker.service docker.socket
-	sudo rm -rf `systemctl cat docker.service | grep -oPm1 "(?<=^#)([^%]+)"` `systemctl cat docker.socket | grep -oPm1 "(?<=^#)([^%]+)"` /usr/bin/docker-compose
-	sudo apt purge docker-engine docker docker.io docker-ce docker-ce-cli -y
-	sudo apt autoremove --purge docker-engine docker docker.io docker-ce -y
-	sudo apt autoclean
-	sudo rm -rf /var/lib/docker /etc/appasudo rmor.d/docker
-	sudo groupdel docker
-	sudo rm -rf /etc/docker /usr/bin/docker /usr/libexec/docker /usr/libexec/docker/cli-plugins/docker-buildx /usr/libexec/docker/cli-plugins/docker-scan /usr/libexec/docker/cli-plugins/docker-app /usr/share/keyrings/docker-archive-keyring.gpg
+
+	# перемещаем файлы
+	mv compose/bin/docker-compose /usr/bin
 }
 # Actions
 $function
