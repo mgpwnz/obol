c2ec98730f5f89c81783ae10b78b396f25e3aee1
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-02-02 16:33:27 +0200

    Update obol_solo.sh

--- obol_solo.sh
+++ obol_solo.sh
@@ -23,16 +23,28 @@ sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install curl wget jq libpq-dev libssl-dev \
 build-essential pkg-config openssl ocl-icd-opencl-dev \
 libopencl-clang-dev libgomp1 -y
-apt update && apt purge docker docker-engine docker.io containerd docker-compose -y
-sleep 1
-rm /usr/bin/docker-compose /usr/local/bin/docker-compose
-sleep 1
-curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
-sleep 20
-curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-sleep 1
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+#docker
+if ! docker --version; then
+		echo -e "${C_LGn}Docker installation...${RES}"
+		sudo apt update && sudo apt upgrade -y
+		sudo apt install curl apt-transport-https ca-certificates gnupg lsb-release -y
+		. /etc/*-release
+		wget -qO- "https://download.docker.com/linux/${DISTRIB_ID,,}/gpg" | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+		echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+		sudo apt update
+		sudo apt install docker-ce docker-ce-cli containerd.io -y
+		docker_version=`apt-cache madison docker-ce | grep -oPm1 "(?<=docker-ce \| )([^_]+)(?= \| https)"`
+		sudo apt install docker-ce="$docker_version" docker-ce-cli="$docker_version" containerd.io -y
+	fi
+ 
+  	echo -e "${C_LGn}Docker Сompose installation...${RES}"
+		sudo apt update && sudo apt upgrade -y
+		sudo apt install wget jq -y
+		local docker_compose_version=`v2.5.0`
+		sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-`uname -s`-`uname -m`"
+		sudo chmod +x /usr/bin/docker-compose
+		. $HOME/.bash_profile
+	
 
 # Actions
 $function
