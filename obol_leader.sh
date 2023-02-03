963635be196c69a229943d570449811327514689
Vitaliy <95574118+mgpwnz@users.noreply.github.com>
2023-02-03 22:53:38 +0200

    +

--- obol_leader.sh
+++ obol_leader.sh
@@ -134,8 +134,9 @@ sed -i -e "s%#MONITORING_PORT_GRAFANA=%MONITORING_PORT_GRAFANA=3008%g" $HOME/cha
 sed -i -e "s%#LIGHTHOUSE_PORT_P2P=%LIGHTHOUSE_PORT_P2P=9100%g" $HOME/charon-distributed-validator-node/.env
 sed -i -e "s%#GETH_PORT_P2P=%GETH_PORT_P2P=32303%g" $HOME/charon-distributed-validator-node/.env
 # Change directory
-cd charon-distributed-validator-node 
-chmod o+w $HOME/charon-distributed-validator-node/.charon
+cd charon-distributed-validator-node && \
+mkdir .charon && \
+chmod o+w
 docker run --rm -v "$(pwd):/opt/charon" obolnetwork/charon:v0.13.0 create enr
 sleep 2
 #backup
